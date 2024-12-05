0b49b9a8a23a9729ef25a9e41d3a0845d764c878
Girgis Michael <93420319+GeorgeMichael74@users.noreply.github.com>
2024-12-05 23:45:24 +0100

    authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/logo2.png";
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 import "./Login.css";
+import { useAuth } from "../context/GlobalState";
 
 const Login = () => {
+   const {user} = useAuth
+   const [email, setEmail] = useState();
+   const [password, setPassword] = useState();
+   const register = (e) => {
+      e.preventDefault();
+      createUserWithEmailAndPassword(auth, email, password);
+   };
+
    return (
       <div className="login">
          <Link to="/">
@@ -13,9 +24,13 @@ const Login = () => {
             <h2>Anmeldung</h2>
             <form>
                <h5>Email</h5>
-               <input type="email" value=""></input>
+               <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+               />
                <h5>Password</h5>
-               <input type="password" value=""></input>
+               <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
                <button className="login-sigInBtn" type="submit">
                   Anmeldung
                </button>
@@ -23,7 +38,9 @@ const Login = () => {
                   Wenn Sie fortfahren, erklären Sie sich mit den Warexpress Fake
                   Clone Nutzungsbedingungen und dem Datenschutz einverstanden.
                </p>
-               <button className="login-registerBtn">Erstellen Sie Ihr Warexpress-Konto</button>
+               <button className="login-registerBtn" onClick={register}>
+                  Erstellen Sie Ihr Warexpress-Konto
+               </button>
             </form>
          </div>
       </div>
