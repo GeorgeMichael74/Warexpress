7c016ffb69aeaa64d7a69e3dfd866d0dbe1bdfa7
Girgis Michael <93420319+GeorgeMichael74@users.noreply.github.com>
2025-01-15 23:51:23 +0100

    Update 2.6

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -7,53 +7,52 @@ import CheckoutProduct from "../checkoutProduct/CheckoutProduct";
 
 const Payment = () => {
    const { basket, user } = useAuth();
+
    return (
       <div className="payment">
-
          <div className="payment-container">
             <h2>
                Checkout (<Link to="/checkout">{basket.length} items</Link>)
             </h2>
 
-            {/* Delivery Address */}
+            {/*===== Delivery Address =====*/}
             <div className="payment-section">
-               <div>
-                  <div className="payment-title">
-                     <h3>Delivery Address</h3>
-                  </div>
+               <div className="payment-title">
+                  <h3>Delivery Address</h3>
+               </div>
 
-                  <div className="payment-address">
-                     <p>{user?.email} </p>
-                     <p>Herforst, Deutschland</p>
-                  </div>
+               <div className="payment-address">
+                  <p>{user?.email} </p>
+                  <p>Herforst, Deutschland</p>
                </div>
             </div>
-            
-            {/* Review Items */}
-            <div className="payment-section">
-                <div className="payment-title">
-                    <h3>Review items and delivery</h3>
-                </div>
-                    {basket.map((item) => (
-                        <CheckoutProduct
-                            key={item.id}
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}/>
-                    ))}
-                <div className="payment-items">
-            
-                </div>
 
+            {/* ===== Review Items ====== */}
+            <div className="payment-section">
+               <div className="payment-title">
+                  <h3>Review items and delivery</h3>
+               </div>
+               <div className="payment-items">
+                  {basket.map((item) => (
+                     <CheckoutProduct
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                     />
+                  ))}
+               </div>
             </div>
 
-            {/* Payment Method */}
-            <div className=""></div>
-
+            {/*===== Payment Method ====*/}
+            <div className="payment-section">
+               <h3>Payment Method</h3>
+               <div className="payment-priceContainer"></div>
+               <div className="payment-details">Stripe magic will go</div>
+            </div>
          </div>
-
       </div>
    );
 };
