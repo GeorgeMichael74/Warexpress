5be85321f51229ed756c1bf141578e76bd6f969e
Girgis Michael <93420319+GeorgeMichael74@users.noreply.github.com>
2025-01-16 21:35:03 +0100

    Update 2.7

--- src/components/subtotal/Subtotal.js
+++ src/components/subtotal/Subtotal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
-import { getBasketTotal } from "../context/AppReducer";
 import { useAuth } from "../context/GlobalState";
+import { getBasketTotal } from "../context/AppReducer";
 import { useNavigate } from "react-router-dom";
 
 import "./Subtotal.css";
@@ -26,7 +26,7 @@ const Subtotal = () => {
             decimalScale={2}
             value={getBasketTotal(basket)}
             displayType={"text"}
-            thousandSeparator={true}
+            thousandSeparator={true}                                
             prefix={"€"}
          />
          <button onClick={()=> Navigate("/payment")}>Proceed to Checkout</button>
